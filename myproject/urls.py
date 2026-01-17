3a655d40d706bc12ea3b7f82c2f8dcaf7ccdcf41
Paras ohri <ohriparas2005@gmail.com>
2026-01-17 18:00:45 +0530

    ok

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+def health_check(request):
+    return JsonResponse({
+        "status": "OK",
+        "message": "Backend is running"
+    })
 
 urlpatterns = [
+    path("", health_check),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('resume.urls')),
