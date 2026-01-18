7525b05746894d320d2aba03307730838e5a7c1f
Paras ohri <ohriparas2005@gmail.com>
2026-01-18 12:20:34 +0530

    ok

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from .views import AuthCheckView
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
+
+    path("auth/check/", AuthCheckView.as_view()),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,22 @@
 from rest_framework.generics import CreateAPIView
 from .serializers import RegisterSerializer
 from django.contrib.auth.models import User
-
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+class AuthCheckView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(
+            {
+                "authenticated": True,
+                "username": request.user.username,
+            },
+            status=status.HTTP_200_OK
+        )
